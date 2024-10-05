668ef2c82fbfca6f3778ba9558039c328dbfd8ff
MrMinh04 <nguyenminhxp2004@gmail.com>
2024-10-05 09:29:22 +0700

    api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\CategoryController;
+use App\Http\Controllers\API\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('category',CategoryController::class);
+// API sản phẩm mới (5 cái)
+Route::get('/products/new', [ProductController::class, 'getNewProducts']);
