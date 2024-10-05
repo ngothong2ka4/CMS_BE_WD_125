327d41e40ca4d833f3eb57278845046de43f0ac5
d-t-a <tuananhdo.bg@gmail.com>
2024-10-05 10:58:38 +0700

    image product

--- app/Http/Controllers/product/ProductVariantController.php
+++ app/Http/Controllers/product/ProductVariantController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\product;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\ProductColor;
+use App\Models\ProductImage;
 use App\Models\Variant;
 use Illuminate\Http\Request;
 
@@ -10,6 +13,39 @@ class ProductVariantController extends Controller
 {
     public function delete(string $id){
         Variant::findOrFail($id)->delete();
+        toastr()->success('Xoá thành công!');
+        return back();
+    }
+
+    public function index(string $id){
+        $product = Product::findOrFail($id);
+        $colors = ProductColor::all();
+        $images = ProductImage::where('id_product', $id)->get();
+        return view('product.product_management.product_images.index', compact('product','images','colors'));
+    }
+
+    public function addImage(Request $request, $id){
+        if($request->hasFile('image')){
+           
+            $image = $request->file('image');
+            $nameImage = time()."_".$image->getClientOriginalName();
+            $image->move('img/products', $nameImage);
+            $path = 'img/products/'. $nameImage;
+            $data = [
+                'id_product' => $id,
+                'id_attribute_color' => $request->id_attribute_color,
+                'link_image' => $path,
+            ];
+             
+           ProductImage::create($data);
+
+           }
+      
+           return back();
+    }
+
+    public function destroy(string $id){
+        ProductImage::findOrFail($id)->delete();
         toastr()->success('Xoá thành công!');
         return back();
     }
---
+++ resources/views/product/product_management/product_images/index.blade.php
@@ -0,0 +1,114 @@
+@extends('layouts.app-theme')
+
+
+@push('scripts')
+     
+@endpush
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="main-card mb-3 card">
+            <div class="card-body">
+
+                <div class="position-relative row form-group mb-5">
+                    <div class="col-md-3 text-md-right"><h3>Tên sản phẩm</h3></div>
+                    <div class="col-md-9 col-xl-8 ">
+                        
+                            <h2>{{$product->name}}</h2>
+                    </div>
+                </div>
+                @foreach(array_unique(array_column($product->variants->toArray(), 'id_attribute_color')) as $key => $color)
+
+                <div class="position-relative row form-group">
+                    <label for="" class="col-md-2 text-md-right col-form-label"><h4>Hình ảnh cho màu 
+                        @foreach($colors as $item)
+                        @if($item->id === $color)
+                           {{$item->name}}
+                        @endif
+                        @endforeach
+                        </h4>
+                    </label>
+                    <div class="col-md-10 col-xl-10">
+                        <ul class="text-break" id="images">
+                            @foreach($images as $image)
+                            @if($color === $image->id_attribute_color)
+                            <li class="float-left d-inline-block mr-2 mb-2" style="position: relative; width: 24%;">
+                                <form action="{{route('delImage',$image->id)}}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit"
+                                        onclick="return confirm('Do you really want to delete this item?')"
+                                        class="btn btn-sm btn-outline-danger border-0 position-absolute">
+                                        X
+
+                                    </button>
+                                </form>
+                                <div style="width: 180px; height: 180px; overflow: hidden;">
+                                    <img width="100%" src="{{$image->link_image}}"
+                                        alt="Image">
+                                </div>
+                            </li>
+                            @endif
+                            @endforeach
+                            <li class="float-left d-inline-block mr-2 mb-2" style="width: 19%;">
+                                <form method="post" enctype="multipart/form-data" action="{{route('delImage',$product->id)}}">
+                                    @csrf
+                                    <input type="hidden" name="id_attribute_color" value="{{$color}}">
+                                    <div style="width: 180; max-height: 180px; overflow: hidden;">
+                                        <img style="width: 100%; cursor: pointer;"
+                                            class="thumbnail"
+                                            data-toggle="tooltip" title="Click to add image" data-placement="bottom"
+                                            src="admin/assets/images/add-image-icon.jpg" alt="Add Image">
+
+                                        <input name="image" type="file" onchange="changeImg(this);this.form.submit()"
+                                            accept="image/x-png,image/gif,image/jpeg"
+                                            class="image form-control-file" style="display: none;">
+
+                                    </div>
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <hr>
+                @endforeach
+                <div class="position-relative row form-group mb-1">
+                    <div class="col-md-9 col-xl-8 offset-md-3">
+                        <a href="#" class="btn-shadow btn-hover-shine btn btn-primary">
+                            <span class="btn-icon-wrapper pr-2 opacity-8">
+                                <i class="fa fa-check fa-w-20"></i>
+                            </span>
+                            <span>OK</span>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@push('scripts')
+    <script>
+  // = = = = = = = = = = = = = = = = changeImg = = = = = = = = = = = = = = = =
+function changeImg(input) {
+    //Nếu như tồn thuộc tính file, đồng nghĩa người dùng đã chọn file mới
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        //Sự kiện file đã được load vào website
+        reader.onload = function (e) {
+            //Thay đổi đường dẫn ảnh
+            $(input).siblings('.thumbnail').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+//Khi click #thumbnail thì cũng gọi sự kiện click #image
+$(document).ready(function () {
+    $('.thumbnail').click(function () {
+        $(this).siblings('.image').click();
+    });
+});
+    </script>
+@endpush
