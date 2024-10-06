3533f8e629e744242d1a21bc85a801941e768350
ngothong2ka4 <ngothong2ka4@gmail.com>
2024-10-06 16:01:22 +0700

    API Auth

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:8|max:32|confirmed',
+        ];
+    }
+    
+    public function messages()
+    {
+        return [
+            'email.required' => 'Email là trường bắt buộc.',
+            'email.email' => 'Email không hợp lệ.',
+            'email.max' => 'Email không được quá 255 ký tự.',
+            'email.unique' => 'Email này đã được sử dụng.',
+            'password.required' => 'Mật khẩu là trường bắt buộc.',
+            'password.min' => 'Mật khẩu phải có ít nhất 8 ký tự.',
+            'password.max' => 'Mật khẩu không được quá 32 ký tự.',
+            'password.confirmed' => 'Mật khẩu xác nhận không khớp.',
+        ];
+    }
+    protected function failedValidation(Validator $validator)
+    {
+        throw new ValidationException($validator);
+    }
+    
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
 Route::apiResource('category',CategoryController::class);
 // API sản phẩm mới (5 cái)
 Route::get('/products/new', [ProductController::class, 'getNewProducts']);
