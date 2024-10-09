ecd1ae2c457590d7cd996ae7d637c970705ffc36
Duyen <duyenctph39530@fpt.edu.vn>
2024-10-09 21:50:03 +0700

    login

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -9,11 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class ProductImage extends Model
 {
     use HasFactory, SoftDeletes;
-<<<<<<< HEAD
-=======
 
->>>>>>> 0ab63c190207585db7fdb0e2974fba2b4b12d74f
-    
     protected $table = 'product_images';
     protected $guarded = [];
 }
