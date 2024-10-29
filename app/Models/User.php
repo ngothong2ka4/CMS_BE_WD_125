f013582cc7a4b0c09f72dafa5be5fb642842dde6
d-t-a <tuananhdo.bg@gmail.com>
2024-10-29 09:13:04 +0700

    điểm tích lũy + điểm tiêu dùng

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,8 @@ class User extends Authenticatable
         'address',
         'role',
         'status',
-        'accum_point'
+        'accum_point',
+        'accumulated_points'
     ];
 
     /**
