51659fb19da8e6b1b7bf3ef893c2ff9f16a175aa
Duyen <duyenctph39530@fpt.edu.vn>
2024-11-30 13:26:32 +0700

    show

--- resources/views/combo/show.blade.php
+++ resources/views/combo/show.blade.php
@@ -12,47 +12,64 @@
                     <h5 class="card-title mb-0">Cập nhật Combo</h5>
                 </div>
                 <div class="card-body">
-                    <form action="{{ route('combo.update',$combos->id) }}" method="POST">
+                    <form action="" method="POST" enctype="multipart/form-data">
                         @csrf
                         @method('PUT')
                         <div>
                             <label class="form-label">Tên combo</label>
-                            <input type="text" class="form-control @error('name') is-invalid @enderror" 
-                                name="name" value="{{$combos->name}}" disabled>
+                            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+                            value="{{$combos->name}}" disabled>
                         </div>
-                        <div class="d-flex my-3">
-                            <label for="basiInput" class="form-label">Chọn sản phẩm </label>
-                            <div class="mx-3">
-                                <select id="id_product" name="id_product[]" class="form-control" disabled multiple="multiple" style="width: 100%;">
-                                    @foreach ($products as $product)
-                                    <option value="{{ $product->id }}" 
-                                        {{ in_array($product->id, $selectedProductIds) ? 'selected' : '' }}>
-                                        {{ $product->name }}
-                                    </option>
-                                @endforeach
+                        <div>
+                            <label class="form-label">Ảnh combo</label>
+                            <img src="{{$combos->image}}" alt=""width="80px">
+                        </div>
+                        <div class="d-flex bd-highlight my-3">
+                            <label for="" class="form-label">Chọn sản phẩm </label>
+                            <div class="w-50 mx-3">
+                                <select id="id_product" name="id_product[]" class="form-control" multiple="multiple"
+                                    style="width: 100%;" disabled>
+                                    @foreach ($productsWithMinValues as $product)
+                                        <option value="{{ $product['id'] }}" data-min-price="{{ $product['min_price'] }}"
+                                            data-min-quantity="{{ $product['min_quantity'] }}"
+                                            {{ in_array($product['id'], $selectedProductIds) ? 'selected' : '' }}>
+                                            {{ $product['name'] }} (Giá: {{ number_format($product['min_price']) }}
+                                            - SL: {{ $product['min_quantity'] }})
+                                        </option>
+                                    @endforeach
                                 </select>
                             </div>
+                            <div class="d-flex w-50">
+                                <div class="d-flex mx-3">
+                                    <label for="" class="form-label">Tổng giá sản phẩm</label>
+                                    <input type="number" class="form-control mb-3" id="total_price" name="total_price"
+                                        readonly>
+                                </div>
+                                <div class="d-flex mx-3">
+                                    <label for="" class="form-label">Số lượng sản phẩm</label>
+                                    <input type="number" class="form-control mb-3" id="minQuantity" name="minQuantity"
+                                         readonly>
+                                </div>
+                            </div>
                         </div>
-
-                        <div>
-                            <label class="form-label">Giá</label>
-                            <input type="number" class="form-control @error('price') is-invalid @enderror" 
-                                name="price" value="{{$combos->price}}" disabled>
+                        <div class="d-flex">
+                            <div class="w-50 mx-3">
+                                <label class="form-label">Giá combo</label>
+                                <input type="number" class="form-control @error('price') is-invalid @enderror"
+                                    name="price" value="{{$combos->price}}" disabled>
+                            </div>
+                            <div class="w-50 mx-3">
+                                <label class="form-label">Số lượng combo</label>
+                                <input type="number" class="form-control @error('quantity') is-invalid @enderror"
+                                    name="quantity" value="{{$combos->quantity }}" disabled>
+                            </div>
                         </div>
                         <div>
                             <label class="form-label">Mô tả</label>
-                            <textarea name="description" id="" class="form-control" cols="30" rows="2" disabled>{{$combos->description}}</textarea>
-                            {{-- <input type="text" class="form-control @error('description') is-invalid @enderror" 
-                                name="" value=""> --}}
-                        </div>
-                        <div>
-                            <label class="form-label">Số lượng</label>
-                            <input type="number" class="form-control @error('quantity') is-invalid @enderror" 
-                                name="quantity" value="{{$combos->quantity}}" disabled>
+                            <textarea name="description" id="" class="form-control" cols="30" rows="2" disabled>{{$combos->description }}</textarea>
                         </div>
                         <div class="mt-3">
-                            {{-- <button type="submit" class="btn btn-success">Gửi</button> --}}
-                            <a href="{{route('combo.index')}}" class="btn btn-success">Quay lại</a>
+                            <a href="{{route('combo.index')}}" class="btn btn-warning">Quay lại</a>
                         </div>
                     </form>
                 </div>
